8e88c206642e6d4a8deaf8290449f4f5917a23f5
Mohamad <mrahmoun93@gmail.com>
2023-06-29 21:37:54 +0200

    remove unused endpoint

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"example.com/main/controllers"
 	"example.com/main/models"
 	"github.com/gin-gonic/gin"
@@ -10,10 +8,6 @@ import (
 
 func main() {
 	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
 
 	r.POST("/activities", controllers.CreateActivity)
 
