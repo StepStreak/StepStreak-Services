e3359a0e70762ddb9bb931c6744f5292e7d48ba4
Mohamad <mrahmoun93@gmail.com>
2023-06-30 01:35:15 +0200

    Acknowledge both messages after processing

--- Worker/main.go
+++ Worker/main.go
@@ -9,35 +9,31 @@ import (
 )
 
 type IncomingMessage struct {
-	ID    int       `json:"id"`
-	Type  string    `json:"type"`
-	Unit  string    `json:"unit"`
-	Value int       `json:"value"`
-	Date  time.Time `json:"date"`
+	ID    int
+	Type  string
+	Unit  string
+	Value int
+	Date  time.Time
 }
 
-type CombinedMessage struct {
-	Steps          int    `json:"steps"`
-	ActiveCalories int    `json:"active_calories"`
-	Date           string `json:"date"`
+type OutgoingMessage struct {
+	Steps          int
+	ActiveCalories int
+	Date           string
 }
-
-var messageMap = make(map[string]map[string]IncomingMessage)
 
 func main() {
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-	}
+	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("Failed to open a channel: %v", err)
-	}
+	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
+	queue, err := ch.QueueDeclare(
 		"messages", // name
 		false,      // durable
 		false,      // delete when unused
@@ -45,9 +41,7 @@ func main() {
 		false,      // no-wait
 		nil,        // arguments
 	)
-	if err != nil {
-		log.Fatalf("Failed to declare a queue: %v", err)
-	}
+	failOnError(err, "Failed to declare a queue")
 
 	outgoingQ, err := ch.QueueDeclare(
 		"activities", // name
@@ -57,79 +51,69 @@ func main() {
 		false,        // no-wait
 		nil,          // arguments
 	)
-	if err != nil {
-		log.Fatalf("Failed to declare a queue: %v", err)
-	}
+	failOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		queue.Name, // queue
+		"",         // consumer
+		false,      // auto-ack
+		false,      // exclusive
+		false,      // no-local
+		false,      // no-wait
+		nil,        // args
 	)
-	if err != nil {
-		log.Fatalf("Failed to register a consumer: %v", err)
-	}
+	failOnError(err, "Failed to register a consumer")
 
-	go func() {
-		for d := range msgs {
-			var msg IncomingMessage
-			err := json.Unmarshal(d.Body, &msg)
-			if err != nil {
-				log.Printf("Error decoding message: %v", err)
-				continue
-			}
+	messageMap := make(map[string]map[string]amqp.Delivery)
 
-			dateStr := msg.Date.Format("2006-01-02")
-			if _, ok := messageMap[dateStr]; !ok {
-				messageMap[dateStr] = make(map[string]IncomingMessage)
-			}
-
-			messageMap[dateStr][msg.Type] = msg
-
-			if len(messageMap[dateStr]) == 2 {
-				stepsMessage, stepsOk := messageMap[dateStr]["Steps"]
-				caloriesMessage, caloriesOk := messageMap[dateStr]["Active Calories"]
-				if stepsOk && caloriesOk {
-					combinedMessage := CombinedMessage{
-						Steps:          stepsMessage.Value,
-						ActiveCalories: caloriesMessage.Value,
-						Date:           dateStr,
-					}
-
-					body, err := json.Marshal(combinedMessage)
-					if err != nil {
-						log.Printf("Error encoding message: %v", err)
-						continue
-					}
-
-					err = ch.Publish(
-						"",             // exchange
-						outgoingQ.Name, // routing key
-						false,          // mandatory
-						false,          // immediate
-						amqp.Publishing{
-							ContentType: "application/json",
-							Body:        body,
-						})
-					if err != nil {
-						log.Printf("Failed to publish a message: %v", err)
-					}
-
-					d.Ack(false)
-					// Assuming you stored delivery tag for the first message, you should ack it as well.
-					// For example: stepsMessage.DeliveryTag.Ack(false)
-
-					delete(messageMap, dateStr)
-				}
-			}
+	for d := range msgs {
+		var msg IncomingMessage
+		json.Unmarshal(d.Body, &msg)
+		date := msg.Date.Format("2006-01-02")
+		if _, ok := messageMap[date]; !ok {
+			messageMap[date] = make(map[string]amqp.Delivery)
 		}
-	}()
+		messageMap[date][msg.Type] = d
 
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	forever := make(chan bool)
-	<-forever
+		if len(messageMap[date]) == 2 {
+			stepsMsg := messageMap[date]["Steps"]
+			activeCaloriesMsg := messageMap[date]["Active Calories"]
+			// acknowledge both messages
+			stepsMsg.Ack(false)
+			activeCaloriesMsg.Ack(false)
+
+			var steps IncomingMessage
+			json.Unmarshal(stepsMsg.Body, &steps)
+			var activeCalories IncomingMessage
+			json.Unmarshal(activeCaloriesMsg.Body, &activeCalories)
+
+			combined := OutgoingMessage{
+				Steps:          steps.Value,
+				ActiveCalories: activeCalories.Value,
+				Date:           date,
+			}
+
+			combinedJson, err := json.Marshal(combined)
+			failOnError(err, "Failed to marshal JSON")
+
+			err = ch.Publish(
+				"",             // exchange
+				outgoingQ.Name, // routing key
+				false,          // mandatory
+				false,          // immediate
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body:        combinedJson,
+				})
+			failOnError(err, "Failed to publish a message")
+
+			delete(messageMap, date)
+		}
+	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
