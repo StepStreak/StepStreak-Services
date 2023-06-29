7f0bf3261cb124a219c9b667e3e257e74ca0be05
Mohamad <mrahmoun93@gmail.com>
2023-06-29 21:39:52 +0200

    adjust producer and handle connection errors

--- messages/producer.go
+++ messages/producer.go
@@ -2,6 +2,7 @@ package messages
 
 import (
 	"encoding/json"
+	"log"
 	"time"
 
 	"example.com/main/models"
@@ -10,18 +11,28 @@ import (
 )
 
 type Activity struct {
-	Steps int16     `json:"steps"`
+	Type  string    `json:"type"`
+	Unit  string    `json:"unit"`
+	Value int16     `json:"value"`
 	Date  time.Time `json:"date"`
 }
 
 func Produce(id uint) {
-	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Printf("Failed to connect to the RabbitMQ: %v", err)
+		return
+	}
 	defer conn.Close()
 
-	ch, _ := conn.Channel()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Printf("Failed to open a channel: %v", err)
+		return
+	}
 	defer ch.Close()
 
-	q, _ := ch.QueueDeclare(
+	q, err := ch.QueueDeclare(
 		"activities",
 		false,
 		false,
@@ -29,14 +40,21 @@ func Produce(id uint) {
 		false,
 		nil,
 	)
+	if err != nil {
+		log.Printf("Failed to declare a queue: %v", err)
+		return
+	}
 
 	var activity models.Activity
-
 	models.DB.Where("id = ?", id).First(&activity)
 
-	body, _ := json.Marshal(activity)
+	body, err := json.Marshal(activity)
+	if err != nil {
+		log.Printf("Failed to marshal the activity: %v", err)
+		return
+	}
 
-	_ = ch.Publish(
+	err = ch.Publish(
 		"",
 		q.Name,
 		false,
@@ -45,4 +63,8 @@ func Produce(id uint) {
 			ContentType: "application/json",
 			Body:        body,
 		})
+	if err != nil {
+		log.Printf("Failed to publish a message: %v", err)
+		return
+	}
 }
