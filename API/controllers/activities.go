a87ad6798ca0955bdfa4805bf476f6daf82f5425
Mohamad <mrahmoun93@gmail.com>
2023-06-30 23:37:34 +0200

    allow processing an array of records

--- API/controllers/activities.go
+++ API/controllers/activities.go
@@ -11,26 +11,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type CreateActivityInput struct {
+type ActivityInput struct {
 	Date  time.Time `json:"date" binding:"required"`
 	Type  string    `json:"type" binding:"required"`
 	Unit  string    `json:"unit" binding:"required"`
 	Value string    `json:"value" binding:"required"`
 }
 
+type CreateActivitiesInput struct {
+	Data []ActivityInput `json:"data" binding:"required"`
+}
+
 func CreateActivity(c *gin.Context) {
-	var input CreateActivityInput
+	var input CreateActivitiesInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	value, _ := strconv.ParseInt(input.Value, 10, 16)
+	for _, activityInput := range input.Data {
+		value, _ := strconv.ParseFloat(activityInput.Value, 64)
 
-	activity := models.Activity{Date: input.Date, Type: input.Type, Unit: input.Unit, Value: int16(value)}
-	models.DB.Create(&activity)
+		var existingActivity models.Activity
+		if err := models.DB.Where("date = ? AND type = ?", activityInput.Date, activityInput.Type).First(&existingActivity).Error; err != nil {
+			activity := models.Activity{Date: activityInput.Date, Type: activityInput.Type, Unit: activityInput.Unit, Value: int32(value)}
+			models.DB.Create(&activity)
+			messages.Produce(activity.ID)
+		}
+	}
 
-	messages.Produce(activity.ID)
-
-	c.JSON(http.StatusOK, gin.H{"data": activity})
+	c.JSON(http.StatusOK, gin.H{"data": input.Data})
 }
